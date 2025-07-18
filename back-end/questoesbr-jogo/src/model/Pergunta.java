549ad7274d6365c20fc977a3d8cecf0d7a97f9ec
Lipedev300 <luizfelipebasso22@gmail.com>
2025-07-18 15:44:59 -0300

    Criada a classe Pergunta, com atributos de texto da pergunta, lista de opções para responder, entre outros. Métodos getters e método para verificar a resposta com base no que o jogador responder

--- back-end/questoesbr-jogo/src/model/Pergunta.java
+++ back-end/questoesbr-jogo/src/model/Pergunta.java
@@ -1,5 +1,37 @@
 package model;
 
-public class Pergunta {
+import java.util.List;
 
+public class Pergunta {
+    private String textoPergunta;
+    private List<String> opcoesResposta;
+    private String respostaCorreta;
+    private String categoria;
+
+    public Pergunta(String textoPergunta, List<String> opcoesResposta, String respostaCorreta, String categoria) {
+        this.textoPergunta = textoPergunta;
+        this.opcoesResposta = opcoesResposta;
+        this.respostaCorreta = respostaCorreta;
+        this.categoria = categoria;
+    }   
+
+    public boolean verificarResposta(String respostaDoJogador) {
+        return this.respostaCorreta.equalsIgnoreCase(respostaDoJogador.trim());
+    }
+
+    public String getTextoPergunta() {
+        return textoPergunta;
+    }   
+
+    public List<String> getOpcoesResposta() {
+        return opcoesResposta;
+    }
+
+    public String getRespostaCorreta() {
+        return respostaCorreta;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
 }
