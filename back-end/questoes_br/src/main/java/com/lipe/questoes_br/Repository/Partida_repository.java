1868166708102f0b08e06aaef176288064068af8
Lipedev300 <luizfelipebasso22@gmail.com>
2025-10-27 21:57:09 -0300

    Criados os repositories das entidades

---
+++ back-end/questoes_br/src/main/java/com/lipe/questoes_br/Repository/Partida_repository.java
@@ -0,0 +1,9 @@
+package com.lipe.questoes_br.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lipe.questoes_br.Entity.Partida;
+
+public interface Partida_repository extends JpaRepository<Partida, Integer> {
+
+}
